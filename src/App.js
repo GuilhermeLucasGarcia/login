756cb33047e6fc69118eef290f2b67afaf632880
GuilherGarcia <47015643+GuilhermeLucasGarcia@users.noreply.github.com>
2023-08-10 15:03:15 -0300

    inicializando front

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="tabelCredenciais" >
+        <div className="tableLabels">
+          <div className="email" >
+            <p>Email</p>
+            <input type="text" placeholder="Email"></input>
+          </div>
+          <div className="senha" >
+            <p>Senha</p>
+            <input type="text" placeholder="Senha"></input>
+          </div>
+          <div className="enviar">
+            <button>Enviar</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
