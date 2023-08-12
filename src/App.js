a1fb44c173e360f83b81019c419d64724d71ff82
GuilherGarcia <47015643+GuilhermeLucasGarcia@users.noreply.github.com>
2023-08-12 00:41:02 -0300

    Add backend

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  } 
+  
+  const handleSenhaChange = (event) => {
+    setSenha(event.target.value);
+  } 
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    try {
+      const userData = {email, senha};
+      const respoonse = await.post('http://localhost:3000/api/users', userData)
+      console.log('Dados enviados', userData);
+
+      setEmail('');
+      setSenha('');
+    } catch(error) {
+      console.error('Erro ao enviar dados:', error);
+    }
+    
+  }
+
   return (
     <div className="App">
       <div className="tabelCredenciais" >
         <div className="tableLabels">
           <div className="email" >
             <p>Email</p>
-            <input type="text" placeholder="Email"></input>
+            <input type="text" placeholder="Email" value={email} onChange={handleEmailChange}></input>
           </div>
           <div className="senha" >
             <p>Senha</p>
-            <input type="text" placeholder="Senha"></input>
+            <input type="password" placeholder="Senha" value={senha} onChange={handleSenhaChange}></input>
           </div>
-          <div className="enviar">
-            <button>Enviar</button>
+          <div className="entrar" onChange={handleSubmit}>
+            <button onClick={handleSubmit}>Entrar</button>
           </div>
         </div>
       </div>
