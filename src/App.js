3c91fce749a1540c0350b6fd4198e215838b1c56
GuilherGarcia <47015643+GuilhermeLucasGarcia@users.noreply.github.com>
2023-08-16 10:16:21 -0300

    Reorganizando pastas

--- src/App.js
+++ src/App.js
@@ -1,50 +1,66 @@
-import React, { useState } from 'react';
+
 import './App.css';
-import axios from 'axios';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from "yup";
+
+
 
 function App() {
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
-
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  } 
   
-  const handleSenhaChange = (event) => {
-    setSenha(event.target.value);
-  } 
+  const handleSubmitCadastro = (values) => console.log(values);
+  const handleSubmit = (values) => console.log(values);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const validationLogin = yup.object().shape({
+    email: yup.string().email("Insira um email válido").required('Este campo é obrigatório'),
+    password: yup.string().min(8, "A senha deve ter 8 caracteres").required('Este campo é obrigatório'),
+  });
 
-    try {
-      const userData = {email, senha};
-      const respoonse = await.post('http://localhost:3000/api/users', userData)
-      console.log('Dados enviados', userData);
-
-      setEmail('');
-      setSenha('');
-    } catch(error) {
-      console.error('Erro ao enviar dados:', error);
-    }
-    
-  }
+  const validationCadastro = yup.object().shape({
+    email: yup.string().email("Insira um email válido").required('Este campo é obrigatório'),
+    password: yup.string().min(8, "A senha deve ter 8 caracteres").required('Este campo é obrigatório'),
+    confirmPassword: yup.string().oneOf([yup.ref("password"), null], 'As senhas não coincidem')
+  })
 
   return (
     <div className="App">
       <div className="tabelCredenciais" >
         <div className="tableLabels">
+        <h1>Login</h1>
+        <Formik initialValues={{}} onSubmit={handleSubmit} validationSchema={validationLogin}>
+          <Form className='login-form'>
           <div className="email" >
-            <p>Email</p>
-            <input type="text" placeholder="Email" value={email} onChange={handleEmailChange}></input>
+            <Field name='email' className='form-field' placeholder='Email'/>
+            <ErrorMessage component='span' name='email'className='form-error'/>
           </div>
           <div className="senha" >
-            <p>Senha</p>
-            <input type="password" placeholder="Senha" value={senha} onChange={handleSenhaChange}></input>
+            <Field name='password' className='form-field' placeholder='senha' type='password'/>
+            <ErrorMessage component='span' name='password'className='form-error'/>          
           </div>
-          <div className="entrar" onChange={handleSubmit}>
-            <button onClick={handleSubmit}>Entrar</button>
+          <div className="entrar">
+            <button type='submit' onSubmit={handleSubmit}>Entrar</button>
           </div>
+          </Form>
+        </Formik>
+        <h1>Registro</h1>
+        <Formik initialValues={{}} onSubmit={handleSubmitCadastro} validationSchema={validationCadastro}>
+          <Form className='login-form'>
+          <div className="email" >
+            <Field name='email' className='form-field' placeholder='Email'/>
+            <ErrorMessage component='span' name='email'className='form-error'/>
+          </div>
+          <div className="senha" >
+            <Field name='password' className='form-field' placeholder='senha' type='text'/>
+            <ErrorMessage component='span' name='password'className='form-error'/>          
+          </div> 
+          <div className="senha" >
+            <Field name='confirmPassword' className='form-field' placeholder='Confirme a senha' type='text'/>
+            <ErrorMessage component='span' name='confirmPassword'className='form-error'/>          
+          </div>
+          <div className="entrar">
+            <button type='submit' onSubmit={handleSubmitCadastro}>Cadastrar</button>
+          </div>
+          </Form>
+        </Formik>
         </div>
       </div>
     </div>
